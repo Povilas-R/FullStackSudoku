6150dc2409009f73a0b7a4a7c07198aff9122d0d
gedasma <gedasma@gmail.com>
2021-03-05 21:22:01 +0200

    Call generate solved for testing purposes.

--- SudokuGeneratorConsole/Program.cs
+++ SudokuGeneratorConsole/Program.cs
@@ -6,12 +6,7 @@ namespace SudokuGeneratorConsole
 	{
 		static void Main(string[] args)
 		{
-			var sudoku = SudokuFactory.FromString(
-				"*9**16*5***6*2**9****8****45**6****7****5****9****1**68****7****5**3*2***3*48**1*|1#--##-#---#-#--#----#----##--#----#----#----#----#--##----#----#--#-#---#-##--#-|", 
-				0);
-			var invalidCells = sudoku.GetInvalidCells();
-			sudoku.Reset();
-			invalidCells = sudoku.GetInvalidCells();
+			var sudoku = SudokuFactory.GenerateSolved(0);
 		}
 	}
 }
